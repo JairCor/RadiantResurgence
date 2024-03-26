ef5d8683dbc4944796e3c276cc9f1117d3211d55
JairCor <jaircoronado2003@gmail.com>
2024-03-25 20:47:33 -0500

    Gameplay Loop Added

--- Radiant Resurgence/Assets/Scripts/DeathHandler.cs
+++ Radiant Resurgence/Assets/Scripts/DeathHandler.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DeathHandler : MonoBehaviour
 {
     [SerializeField] private GameObject deathScreen;
     public void HandleDeath()
     {
+        Time.timeScale = 0;
         deathScreen.SetActive(true);
         Debug.Log("Character has died. Death screen should appear.");
+    }
+    public void Awake()
+    {
+        deathScreen.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Respawn()
+    {
+
+        SceneManager.LoadScene("Game");
+        Time.timeScale = 1;
+        
     }
 }
